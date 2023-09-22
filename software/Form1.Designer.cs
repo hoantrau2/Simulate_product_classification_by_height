4ce8b6c5c642905aeca2826b9539cfbdfc7a6a09
hoantrau2 <hoan.duongmybk852002@hcmut.edu.vn>
2023-09-23 05:34:49 +0700

    classify by height

---
+++ software/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace CS_to_S7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.IP = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Running_Label = new System.Windows.Forms.Label();
+            this.Stop_Label = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CTH = new System.Windows.Forms.Label();
+            this.CTM = new System.Windows.Forms.Label();
+            this.CTS = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // IP
+            // 
+            this.IP.Location = new System.Drawing.Point(285, 331);
+            this.IP.Margin = new System.Windows.Forms.Padding(4);
+            this.IP.Name = "IP";
+            this.IP.Size = new System.Drawing.Size(131, 22);
+            this.IP.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(285, 384);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(73, 353);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 54);
+            this.button2.TabIndex = 34;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(165, 353);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 54);
+            this.button3.TabIndex = 35;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(107, 416);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 41);
+            this.button4.TabIndex = 36;
+            this.button4.Text = "Reset Counter";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Running_Label
+            // 
+            this.Running_Label.AutoSize = true;
+            this.Running_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Running_Label.ForeColor = System.Drawing.Color.Green;
+            this.Running_Label.Location = new System.Drawing.Point(106, 285);
+            this.Running_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Running_Label.Name = "Running_Label";
+            this.Running_Label.Size = new System.Drawing.Size(109, 29);
+            this.Running_Label.TabIndex = 38;
+            this.Running_Label.Text = "Running";
+            // 
+            // Stop_Label
+            // 
+            this.Stop_Label.AutoSize = true;
+            this.Stop_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Stop_Label.ForeColor = System.Drawing.Color.Red;
+            this.Stop_Label.Location = new System.Drawing.Point(130, 285);
+            this.Stop_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Stop_Label.Name = "Stop_Label";
+            this.Stop_Label.Size = new System.Drawing.Size(67, 29);
+            this.Stop_Label.TabIndex = 39;
+            this.Stop_Label.Text = "Stop";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Peru;
+            this.panel1.Location = new System.Drawing.Point(118, 13);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(35, 28);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Peru;
+            this.panel2.Location = new System.Drawing.Point(265, 12);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(35, 28);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Peru;
+            this.panel3.Location = new System.Drawing.Point(408, 13);
+            this.panel3.Margin = new System.Windows.Forms.Padding(4);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(35, 28);
+            this.panel3.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(22, 20);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Sensor_1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(169, 20);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Sensor_2";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(308, 20);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 20);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Sensor_3";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(63, 65);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(93, 20);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Sensor_in";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(231, 66);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(104, 20);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Sensor_out";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Peru;
+            this.panel4.Location = new System.Drawing.Point(180, 66);
+            this.panel4.Margin = new System.Windows.Forms.Padding(4);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(35, 28);
+            this.panel4.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Peru;
+            this.panel5.Location = new System.Drawing.Point(361, 66);
+            this.panel5.Margin = new System.Windows.Forms.Padding(4);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(35, 28);
+            this.panel5.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(62, 139);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(170, 29);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Counter Long";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(62, 181);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(205, 29);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "Counter Medium";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(68, 219);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(174, 29);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Counter Short";
+            // 
+            // CTH
+            // 
+            this.CTH.AutoSize = true;
+            this.CTH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CTH.Location = new System.Drawing.Point(292, 139);
+            this.CTH.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CTH.Name = "CTH";
+            this.CTH.Size = new System.Drawing.Size(36, 25);
+            this.CTH.TabIndex = 37;
+            this.CTH.Text = "00";
+            // 
+            // CTM
+            // 
+            this.CTM.AutoSize = true;
+            this.CTM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CTM.Location = new System.Drawing.Point(292, 181);
+            this.CTM.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CTM.Name = "CTM";
+            this.CTM.Size = new System.Drawing.Size(36, 25);
+            this.CTM.TabIndex = 38;
+            this.CTM.Text = "00";
+            // 
+            // CTS
+            // 
+            this.CTS.AutoSize = true;
+            this.CTS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CTS.Location = new System.Drawing.Point(292, 219);
+            this.CTS.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CTS.Name = "CTS";
+            this.CTS.Size = new System.Drawing.Size(36, 25);
+            this.CTS.TabIndex = 39;
+            this.CTS.Text = "00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 553);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.IP);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.CTS);
+            this.Controls.Add(this.Running_Label);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.CTM);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.CTH);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Stop_Label);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox IP;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label Running_Label;
+        private System.Windows.Forms.Label Stop_Label;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label CTH;
+        private System.Windows.Forms.Label CTM;
+        private System.Windows.Forms.Label CTS;
+    }
+}
+
---
+++ software/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using S7.Net;
+using S7.Net.Types;
+
+namespace CS_to_S7
+{
+    public partial class Form1 : Form
+    {
+        byte[] Output;
+        byte[] Input;
+
+        Plc PLC;
+        public Form1()
+        {
+            InitializeComponent();
+            Running_Label.Visible = false;
+            Stop_Label.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            PLC = new Plc(CpuType.S71200, IP.Text,0,0);
+            if(PLC.Open() == ErrorCode.NoError)
+            {
+                MessageBox.Show("Connect Successfully");
+                timer1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Wrong IP address");
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Input = PLC.ReadBytes(DataType.Input, 0, 0, 2);
+                panel1.BackColor = (Input[0].SelectBit(3) == true) ? Color.Blue : Color.LightGray;
+                panel2.BackColor = (Input[0].SelectBit(4) == true) ? Color.Blue : Color.LightGray;
+                panel3.BackColor = (Input[0].SelectBit(5) == true) ? Color.Blue : Color.LightGray;
+                panel4.BackColor = (Input[0].SelectBit(6) == true) ? Color.Blue : Color.LightGray;
+                panel5.BackColor = (Input[0].SelectBit(7) == true) ? Color.Blue : Color.LightGray;
+
+                
+                Output = PLC.ReadBytes(DataType.Output, 0, 0, 1);
+                //panel14.BackColor = (Output[0].SelectBit(0) == true) ? Color.Blue : Color.LightGray;
+                //panel13.BackColor = (Output[0].SelectBit(1) == true) ? Color.Blue : Color.LightGray;
+                //panel12.BackColor = (Output[0].SelectBit(2) == true) ? Color.Blue : Color.LightGray;
+                //panel11.BackColor = (Output[0].SelectBit(3) == true) ? Color.Blue : Color.LightGray;
+                //panel10.BackColor = (Output[0].SelectBit(4) == true) ? Color.Blue : Color.LightGray;
+                //panel9.BackColor  = (Output[0].SelectBit(5) == true) ? Color.Blue : Color.LightGray;
+                //panel8.BackColor  = (Output[0].SelectBit(6) == true) ? Color.Blue : Color.LightGray;
+
+                Running_Label.Visible = (Output[0].SelectBit(1) == true) ? true : false;
+                Stop_Label.Visible = (Output[0].SelectBit(1) == false) ? true : false;
+
+                CTH.Text = PLC.Read("MD0").ToString();
+                CTM.Text = PLC.Read("MD4").ToString();
+                CTS.Text = PLC.Read("MD8").ToString();
+            }
+            catch
+            {
+                PLC.Close();
+                PLC.Open();
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            if (PLC.Open() == ErrorCode.NoError)
+            {
+                for (int i = 30; i >= 0; i--)
+                {
+                    PLC.Write("I0.0", 1);
+                    PLC.Write("I0.0", 0);
+                }
+                Running_Label.Visible = true;
+                Stop_Label.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("PLC was not connected ");
+            }
+        }
+        private async void button3_Click(object sender, EventArgs e)
+        {
+
+            if (PLC.Open() == ErrorCode.NoError)
+            {
+                for (int i = 30; i >= 0; i--)
+                {
+                    PLC.Write("I0.1", 1);
+                    PLC.Write("I0.1", 0);
+                }
+                Running_Label.Visible = false;
+                Stop_Label.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("PLC was not connected ");
+            }
+        }
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            if (PLC.Open() == ErrorCode.NoError)
+            {
+                for (int i = 30; i>=0; i--) {
+                    PLC.Write("I0.2", 1);
+                    PLC.Write("I0.2", 0);
+                }
+            }
+            else
+            {
+                MessageBox.Show("PLC was not connected ");
+            }
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
